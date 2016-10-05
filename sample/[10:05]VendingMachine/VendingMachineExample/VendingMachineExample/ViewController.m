37a640a2c7fad064ab448cef384311359c04c8ae
jakouk <jakoriaty@gmail.com>
2016-10-05 10:15:28 +0900

    Create VendingMachineExample

--- sample/[10:05]VendingMachine/VendingMachineExample/VendingMachineExample/ViewController.m
+++ sample/[10:05]VendingMachine/VendingMachineExample/VendingMachineExample/ViewController.m
@@ -10,13 +10,44 @@
 
 @interface ViewController ()
 
+@property (nonatomic) NSArray *itemImgName;
+@property (nonatomic) NSDictionary *itemData;
+@property (nonatomic) NSArray *costData;
+
+@property (nonatomic, weak) UIView *itemContainerView;
+@property (nonatomic) NSMutableArray *itemViews;
+
+@property (nonatomic, weak) UIView *dispalyView;
+@property (nonatomic, weak) UILabel *displayLb;
+
+@property (nonatomic, weak) UIView *inputView2;
+@property (nonatomic) NSMutableArray *inputBtns;
+
+
+//남은 돈
+@property (nonatomic, assign)NSInteger remaindMoney;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.itemImgName = @[@"island1",@"island2",@"island3.jpg",@"island4.jpg"];
+    self.itemData = @{@"sum1":@"1000",@"sum2":@"2000",@"sum3":@"3000",@"sum4":@"4000"};
+    self.costData = @[@"1000",@"500",@"100",@"반환"];
+    self.itemViews = [[NSMutableArray alloc] init];
+    self.inputBtns = [[NSMutableArray alloc] init];
+    
+    [self createView];
+    [self updateLayout];
+}
+
+-(void)createView{
+    
+}
+
+-(void)updateLayout{
+    
 }
 
 
