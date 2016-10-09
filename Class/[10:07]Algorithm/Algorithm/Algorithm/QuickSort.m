d7517b1e4019ad31c55bf97a8c2bd49983c1a26f
jakouk <jakoriaty@gmail.com>
2016-10-09 21:06:51 +0900

    Update Algorithm(QuickSort comment)

--- Class/[10:07]Algorithm/Algorithm/Algorithm/QuickSort.m
+++ Class/[10:07]Algorithm/Algorithm/Algorithm/QuickSort.m
@@ -14,17 +14,21 @@
     
     NSMutableArray *unSortedMutable = [[NSMutableArray alloc]initWithArray:unSortedList];
     
-    // quick 정렬될 배열
+    // quickSort 될 배열
     NSMutableArray *sortedArray = [[NSMutableArray alloc]init];
     
     NSMutableArray *lessArray = [[NSMutableArray alloc]init];
     NSMutableArray *greaterArray = [[NSMutableArray alloc]init];
     
     if([unSortedList count] <1){
+        
         return nil;
     }
     
     //pivot 정하기
+    //unSortedMutable의 Index 0번째 값을
+    //pivotValue로 만들고 그 값을 unSortedMutable에서 지운다
+    //pivotValue 보다 작으면 lessArray // 그 이외는 greaterArray
     NSNumber *pivotValue = [[NSNumber alloc]init];
     
     pivotValue = unSortedMutable[0];
@@ -42,7 +46,9 @@
     }
     
     //병합
-   
+    //결국 모든 값은 한번씩은 pivotValue가 되고 그 상태에서 정렬된 배열에 들어간다.
+    
+    NSLog(@"pivotValue :%@",pivotValue);
     
     [sortedArray addObjectsFromArray:[self quickSort: lessArray]];
     
