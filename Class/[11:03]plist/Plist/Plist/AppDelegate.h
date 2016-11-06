791d1ff96e919c41e0ea098843efcac964d3da7e
jakouk <jakoriaty@gmail.com>
2016-11-06 23:32:45 +0900

    Create plist , Update LoginPage( plist )

---
+++ Class/[11:03]plist/Plist/Plist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Plist
+//
+//  Created by jakouk on 2016. 11. 3..
+//  Copyright © 2016년 jakouk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- sample/[11:02]LoginPage/LoginPage/LoginPage/DataCenter.m
+++ sample/[11:02]LoginPage/LoginPage/LoginPage/DataCenter.m
@@ -36,8 +36,15 @@
     if (![userID isEqualToString:[self.userDefault objectForKey:@"ID"]]) {
         [self.userDefault setObject:userID forKey:@"ID"];
         [self.userDefault setObject:userPW forKey:@"PW"];
+        
+        NSMutableDictionary *userData = [[NSMutableDictionary alloc] init];
+        [userData setValue:userID forKey:@"ID"];
+        [userData setValue:userPW forKey:@"PW"];
+        
+        [self saveUserData:userData];
         isJoinAble = YES;
-    } 
+    }
+    
     return isJoinAble;
 }
 
@@ -49,6 +56,21 @@
     
     if ([userID isEqualToString:[self.userDefault objectForKey:@"ID"]] && [userPW isEqualToString:[self.userDefault objectForKey:@"PW"]]) {
         isLoinPossible = YES;
+    }
+    
+    NSArray *userDatas = [self loadUserData];
+    NSLog(@"userDatas %ld", userDatas.count);
+    
+    for (NSDictionary *userData in userDatas) {
+        //이 부분에서 오류
+        NSString *savedUserID = [userData objectForKey:@"ID"];
+        NSString *savedUserPW = [userData objectForKey:@"PW"];
+        NSLog(@"savedUserID %@ , savedUserPW %@", savedUserID, savedUserPW);
+        
+        if ( [userID isEqualToString:savedUserID] && [userPW isEqualToString:savedUserPW] ) {
+            isLoinPossible = YES;
+        }
+        
     }
     
     return isLoinPossible;
@@ -82,12 +104,54 @@
     return id;
 }
 
+//가입하면서 plist에 저장함
 - (void)saveUserData:(NSDictionary *)data {
     
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *basePath = [paths objectAtIndex:0];
+    NSString *docuPath = [basePath stringByAppendingString:@"FriendList.plist"];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:docuPath]) {
+        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FriendList" ofType:@"plist"];
+        [fileManager copyItemAtPath:bundlePath toPath:docuPath error:nil];
+    }
+    
+    //데이터를 저장하는 부분에서 문제가 있는것 같음.
+    NSMutableDictionary *dic= [NSMutableDictionary dictionaryWithContentsOfFile:docuPath];
+    NSMutableArray *userDatas = [[NSMutableArray alloc] init];
+    
+    if ( [dic objectForKey:@"userData"] != nil) {
+        [userDatas addObject:[dic objectForKey:@"userData"]];
+    }
+    
+    [userDatas addObject:data];
+    
+    NSLog(@"saveUserData of userDatas %ld",userDatas.count);
+    
+    [dic setObject:userDatas forKey:@"userData"];
+    [dic writeToFile:docuPath atomically:NO];
 }
 
-//- (NSArray *)loadUserData {
-//    
-//}
+//로그인할때 plist의 정보 search
+- (NSArray *)loadUserData {
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *basePath = [paths objectAtIndex:0];
+    NSString *docuPath = [basePath stringByAppendingString:@"FriendList.plist"];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:docuPath]) {
+        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FriendList" ofType:@"plist"];
+        [fileManager copyItemAtPath:bundlePath toPath:docuPath error:nil];
+    }
+    
+    NSMutableDictionary *dic= [NSMutableDictionary dictionaryWithContentsOfFile:docuPath];
+    NSMutableArray *userData = [dic objectForKey:@"userData"];
+    
+    NSLog(@"loadUserData userData %ld",userData.count);
+    
+    return userData;
+}
 
 @end
